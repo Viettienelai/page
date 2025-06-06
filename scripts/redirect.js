d6bd6a05f425c652ebfc24a90683c2be8dc10ac9
Viettienelai <vtkjbtlvkh@gmail.com>
2025-06-06 23:34:45 +0700

    Delete scripts directory

--- scripts/redirect.js
+++
@@ -1,8 +0,0 @@
-const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
-
-// Chuyển hướng dựa trên thiết bị
-if (isMobile) {
-  window.location.href = './mobile.html';
-} else {
-  window.location.href = './desktop.html';
-}
